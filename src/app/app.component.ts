76e1fabf16f11f01d655e39d8da861ce652bec8c
Wicho Perez <luis.carrillo.pereez@gmail.com>
2020-03-31 20:35:21 -0600

    Sockets-Client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import Ws from "@adonisjs/websocket-client";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'Sockets-Client';
+export class AppComponent implements OnInit {
+  title = "Sockets-Client";
+  ws: any;
+  chat: any;
+  messages: string[] = [];
+  text: string;
+
+  ngOnInit(): void {
+    this.ws = Ws("ws://localhost:3333", {
+      path: "ws"
+    });
+
+    this.ws.connect();
+    this.chat = this.ws.subscribe("chat");
+
+    this.chat.on("message", (data: any) => {
+      this.messages.push(data);
+    });
+  }
+
+  sendMessage(): void {
+    this.chat.emit("message", this.text);
+    this.messages.push(this.text);
+    this.text = "";
+  }
 }
